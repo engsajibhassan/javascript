a677950f52b779ef90896c81935fc45f36832621
engsajibhassan <engsajibhassan@gmail.com>
2023-08-01 20:04:52 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// let carName; 
+// price, model, year, city;
+const carName = 'Volvo';
+price = '1cr';
+// model = 'New';
+// year = '2020';
+// city = 'Dhaka'
+console.log('Brand is ' + carName);
+console.log('Price is ' + price);
