85b565c711f0b591f3f7f70ea84ece1e02723006
engsajibhassan <engsajibhassan@gmail.com>
2023-08-03 20:29:07 +0600

    studied till operator

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
-// let carName; 
-// price, model, year, city;
-const carName = 'Volvo';
-price = '1cr';
-model = 'New';
-// year = '2020';
-// city = 'Dhaka'
-console.log('Brand is ' + carName);
-console.log('Price is ' + price);
-console.log('Model is ' + model);
+const temp = 1;
+
